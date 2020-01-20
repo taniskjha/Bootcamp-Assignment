07d863b8a94dee906b7b5a9901fe257d3cf65405
Tanisk jha <jhatanishqq@outlook.com>
2020-01-20 12:12:37 +0530

    problems

---
+++ higherOrderFunctions/3.js
@@ -0,0 +1,16 @@
+// Create a function called map that takes two inputs:
+// an array of numbers (a list of numbers)
+// a 'callback' function - a function that is applied to each element of the array (inside of the function 'map')
+// Have map return a new array filled with numbers that are the result of using the 'callback' function on each element of the input array.
+// map([1,2,3,4,5], multiplyByTwo); //-> [2,4,6,8,10]
+// multiplyByTwo(1); //-> 2
+// multiplyByTwo(2); //-> 4
+
+// Challenge 3
+function map(array, callback) {
+
+  return array.map(mul => mul * 2)
+
+}
+
+console.log(map([1, 2, 3] ));
