dbf959f0aec3942c3b126780faa4d55fecf2bfbc
Tanisk jha <jhatanishqq@outlook.com>
2020-01-21 11:43:46 +0530

    first commit

---
+++ XHR-Object-Methods-AND-Working-With-Text/XHR.js
@@ -0,0 +1,5 @@
+document.getElementById('button').addEventListener('click', loadData);
+
+function loadData() {
+  // create an xhr number
+}
---
+++ higherOrderFunctions/4.js
@@ -0,0 +1,29 @@
+// Challenge 4
+// The function forEach takes an array and a callback, and runs the callback on each element of the array. forEach does not return anything.
+// let alphabet = ''; 
+// const letters = ['a', 'b', 'c', 'd']; 
+// forEach(letters, function(char) 
+// {alphabet += char}); 
+// console.log(alphabet);   //prints 'abcd'
+
+
+// // Challenge 4
+// function forEach(array, callback) {
+  
+
+
+// }
+
+// "see for yourself if your forEach works!"
+
+function outer() {
+  var counter = 0;
+  function x() {
+    
+  return counter ++ 
+  } return x
+}
+const y = outer()
+y();
+y();
+
