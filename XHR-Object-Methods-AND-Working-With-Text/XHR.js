1049ae76ef1eaa773194ae582ce146310ecee4eb
Tanisk jha <jhatanishqq@outlook.com>
2020-01-21 12:41:09 +0530

    final commit

--- XHR-Object-Methods-AND-Working-With-Text/XHR.js
+++ XHR-Object-Methods-AND-Working-With-Text/XHR.js
@@ -15,10 +15,19 @@ function loadData() {
 
   // When everything is ready it's going to call this onload and we want to make sure it's a status of 200
 
-  xhr.onload = function() {
-    if (this.status === 200) {
-      console.log(this.responseText)
-    }
+
+  xhr.onload = function(){	
+    console.log('READYSTATE', xhr.readyState);	
+    if(this.status === 200) {	
+      // console.log(this.responseText);	
+      document.getElementById('output').innerHTML = `<h3>${this.responseText}</h3>`;	
+    }	
+  }
+
+
+// display   
+  xhr.onerror = function() {
+    console.log('Request error')
   }
 // finally we have to do send for to actually send to make it work
 
@@ -29,8 +38,8 @@ function loadData() {
 
 
 // readystate value 
-// o - request not initialized
-// server connection established
-// request recieved 
-// processing request
-// request finished is ready 
+// o- request not initialized
+// 1- server connection established
+// 2- request recieved 
+// 3- processing request
+// 4- request finished is ready 
