edd096e217a3b2d2b1e3f0a63b8ab5d2dca7a5e6
Tanisk jha <jhatanishqq@outlook.com>
2020-01-25 12:47:50 +0530

    commit

---
+++ Assignment/remainder-of-two-numbers-without-using-modulus-(%)operator.js
@@ -0,0 +1,14 @@
+// 5) Write a program to find remainder of two numbers without using modulus (%)operator
+
+
+function modulus(a,b) {
+// Step 1 - Find quotient
+
+
+var p = q * (a / b);
+
+return a - p;
+
+
+} modulus (5, 2)
+
