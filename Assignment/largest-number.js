3e70f99cd3e4f9d9579085509332ad4b7cd31f7b
tanisk jha <jhatanishqq@outlook.com>
2020-01-26 16:00:06 +0530

    last commit

---
+++ Assignment/largest-number.js
@@ -0,0 +1,7 @@
+// Write a program to find the largest number among three numbers
+
+function findLargest(a,b,c) {
+    
+    return Math.max(a,b,c) // I love es6 methods
+ 
+}  findLargest(2,50,56); 
---
+++ Assignment/leap-year.js
@@ -0,0 +1,8 @@
+//  Write a program to Check Whether the given year is a leap year or not
+
+function checkLeapYear(y) {
+    if (y % 4 === 0) {
+    return "i'm leap"
+    } 
+    return "i'm not"
+} checkLeapYear(2020)
