a71031eecf55f271a185dd3efc224f84b1166d9d
tanisk jha <jhatanishqq@outlook.com>
2020-01-26 15:34:40 +0530

    last commit

--- Assignment/swapping-of-two-array.js
+++ Assignment/swapping-of-two-array.js
@@ -4,7 +4,10 @@ var a = [2,3,5];
 
 var b = [6,7,8];
 
+var c = a; // c got the value of a
 
-var a = b;
+var b = a; // b got the value of a
+var a = c; // a got the valud of c
 
-var b = a;
+console.log(a)
+console.log(b)
