43ecdf7bd2b64b4c6a08f7917b4b226263544927
tanisk jha <jhatanishqq@outlook.com>
2020-01-26 15:45:17 +0530

    last commit

---
+++ Assignment/vowel-or-consonant.js
@@ -0,0 +1,12 @@
+//  Write a program to check whether given character is vowel or consonant
+
+function check(me) {
+if (me == 'a' || me == 'e' || me == 'i'|| me == 'o'|| me == 'u' ) {
+    console.log('vowel')
+
+
+} return 'consonant'  }
+
+check(a);
+check(z);
+
