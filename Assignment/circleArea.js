b5c69b85df1c08365b3d1c4632531dd60db325a4
tanisk jha <jhatanishqq@outlook.com>
2020-01-29 18:43:13 +0530

    last commit

--- Assignment/circleArea.js
+++ Assignment/circleArea.js
@@ -1,7 +1,12 @@
 // Write a program to find area and circumference of circle
 
-function findArea(radius) {
 
-    return Math.PI * Math.pow(radius, 2) 
+var readlineSync = require('readline-sync');
+
+var question = Number(readlineSync.question('enter the circle radius'))
+
+function findArea(question) {
+
+    return Math.PI * Math.pow(question, 2) 
     
 } findArea(50);
