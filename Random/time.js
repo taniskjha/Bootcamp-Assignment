2005b939c3aee21a36f18e96152a3a2e15606b93
tanisk jha <jhatanishqq@outlook.com>
2020-02-04 16:09:13 +0530

    last-commit

---
+++ Random/time.js
@@ -0,0 +1,5 @@
+var x = Date.now()
+
+// 1580812591304
+
+new Date(x);
--- Recurscion/Algorithm_R_Problem/Recursive_method.js
+++ Recurscion/Algorithm_R_Problem/Recursive_method.js
@@ -2,55 +2,76 @@
 
 // INPUT - ABCD   // OUTPUT - [ '01000001', '01000010', '00100000', '01000011', '01000100' ]
 
-var​ ascii = {    
-         A   :   ​65​  ,   
-         B   :   ​66​  ,   
-         C   :   ​67​  ,   
-         D   :   ​68​  ,    
-         E   :   ​69​  ,    
-         F   :   ​70​  ,    
-         G   :   ​71​  ,   
-         H   :   ​72​  ,    I   :   ​73​  ,    J   :   ​74​  ,    K   :   ​75​  ,    L   :   ​76​  ,    M   :   ​77​  ,    N   :   ​78​  ,    O   :   ​79​  ,    P   :   ​80​  ,    Q   :   ​81​  ,    R   :   ​82​  ,    S   :   ​83​  ,    T   :   ​84​  ,    U   :   ​85​  ,    V   :   ​86​  ,    W   :   ​87​  ,    X   :   ​88​  ,    Y   :   ​89​  ,    Z   :   ​90​  ,    a   :   ​97​  ,    b   :   ​98​  ,    c   :   ​99​  ,    d   :   ​100​ ,    e   :   ​101​ ,    f   :   ​102​ ,    g   :   ​103​ ,    h   :   ​104​ ,    i   :   ​105​ ,
-   
-         j   :   ​106​ ,    k   :   ​107​ ,    l   :   ​108​ ,    m   :   ​109​ ,    n   :   ​110​ ,    o   :   ​111​ ,    p   :   ​112​ ,    q   :   ​113​ ,    r   :   ​114​ ,    s   :   ​115​ ,    t   :   ​116​ ,    u   :   ​117​ ,    v   :   ​118​ ,    w   :   ​119​ ,    x   :   ​120​ ,    y   :   ​121​ ,    z   :   ​122​ ,​' '​ :   ​32​  ,​':'​ :   ​58​  ,​';'​ :   ​59​  ,​'<'​ :   ​60​  ,​'='​ :   ​61​  ,​'>'​ :   ​62​  ,​'?'​ :   ​63​  ,​'@'​ :   ​64​  ,​'/'​ :   ​47​  ,​'0'​ :   ​48​  ,​'1'​ :   ​49​  ,​'2'​ :   ​50​  ,​'3'​ :   ​51​  ,​'4'​ :   ​52​  ,​'5'​ :   ​53​  ,​'6'​ :   ​54​  ,​'7'​ :   ​55​  ,​'8'​ :   ​56​  ,​'9'​ :   ​57}
+// var​ ascii = {    
+//          A   :   ​65​  ,   
+//          B   :   ​66​  ,   
+//          C   :   ​67​  ,   
+//          D   :   ​68​  ,    
+//          E   :   ​69​  ,    
+//          F   :   ​70​  ,    
+//          G   :   ​71​  ,   
+//          H   :   ​72​  ,    I   :   ​73​  ,    J   :   ​74​  ,    K   :   ​75​  ,    L   :   ​76​  ,    M   :   ​77​  ,    N   :   ​78​  ,    O   :   ​79​  ,    P   :   ​80​  ,    Q   :   ​81​  ,    R   :   ​82​  ,    S   :   ​83​  ,    T   :   ​84​  ,    U   :   ​85​  ,    V   :   ​86​  ,    W   :   ​87​  ,    X   :   ​88​  ,    Y   :   ​89​  ,    Z   :   ​90​  ,    a   :   ​97​  ,    b   :   ​98​  ,    c   :   ​99​  ,    d   :   ​100​ ,    e   :   ​101​ ,    f   :   ​102​ ,    g   :   ​103​ ,    h   :   ​104​ ,    i   :   ​105​ ,
 
-        
-         // Step 1- traverse the string 
+//          j   :   ​106​ ,    k   :   ​107​ ,    l   :   ​108​ ,    m   :   ​109​ ,    n   :   ​110​ ,    o   :   ​111​ ,    p   :   ​112​ ,    q   :   ​113​ ,    r   :   ​114​ ,    s   :   ​115​ ,    t   :   ​116​ ,    u   :   ​117​ ,    v   :   ​118​ ,    w   :   ​119​ ,    x   :   ​120​ ,    y   :   ​121​ ,    z   :   ​122​ ,​' '​ :   ​32​  ,​':'​ :   ​58​  ,​';'​ :   ​59​  ,​'<'​ :   ​60​  ,​'='​ :   ​61​  ,​'>'​ :   ​62​  ,​'?'​ :   ​63​  ,​'@'​ :   ​64​  ,​'/'​ :   ​47​  ,​'0'​ :   ​48​  ,​'1'​ :   ​49​  ,​'2'​ :   ​50​  ,​'3'​ :   ​51​  ,​'4'​ :   ​52​  ,​'5'​ :   ​53​  ,​'6'​ :   ​54​  ,​'7'​ :   ​55​  ,​'8'​ :   ​56​  ,​'9'​ :   ​57}
 
-         // Step 2- convert every character to their charcode
 
-         // Step 3- convert the charcode to binary
+// Step 1- traverse the string 
 
-         // Step 4- push it into an array and add the left 0s
+// Step 2- convert every character to their charcode
 
-         // Step 5- return a string separated by space
+// Step 3- convert the charcode to binary
 
-         var ascii = {
-          A: 65,
-          B: 66,
-          C: 67,
-          D: 68,
+// Step 4- push it into an array and add the left 0s
 
-        }        
+// Step 5- return a string separated by space
+
+var ascii = {
+  A: 65,
+  B: 66,
+  C: 67,
+  D: 68,
+
+}
 
 
 
 function foo(binaryToString) {
-let bi = [];
+  let bi = [];
 
-binaryToString.split('').forEach(function(element) {
-  k = '';
-  for (const prop in ascii) {
-    if (prop == element) {
-      var x = ascii[prop];
-      break;
+  binaryToString.split('').forEach(function (element) {
+    k = '';
+    for (const prop in ascii) {
+      if (prop == element) {
+        var x = ascii[prop];
+        bi.push(x)
+      }
     }
+
+  });
+
+  // recursion logic
+  for (var i = 0; i < bi.length; i++ ) 
+
+  let arr = [];
+  function f(n) {
+
+    
+
+    if (n <= 1) {
+      arr.push(n);
+    }
+
+    else {
+
+      f(Math.floor(n / 2));
+      arr.push(n % 2);
+    }
+    
+
   }
-  bi.push(binaryToString.padStart(8, '0'));
-});
+f(25)
+console.log(arr) 
 
-
-
-}        
+}
 
 foo('ABCD')
