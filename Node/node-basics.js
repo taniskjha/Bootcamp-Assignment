5d284a25ec2e7efd45508841da61b89d5b2072e8
tanisk jha <jhatanishqq@outlook.com>
2020-02-10 19:07:57 +0530

    last commit

---
+++ Node/node-basics.js
@@ -0,0 +1,35 @@
+// Q- What is nodeJs
+
+// Ans - Node.js is a cross-platform JavaScript runtime environment that allows developers to build server-side and network applications with JavaScript.
+
+// Q- Why use Nodejs 
+
+// Ans - primary use of nodeJs is non-blocking I/O, event-driven servers, due to its single-threaded nature. It's used for traditional web sites and back-end API services.
+
+// Q - What is nodeJs event Loop ?
+
+// Ans - The event loo allows Node.js to perform non-blocking I/O operations — despite the fact that JavaScript is single-threaded
+
+// Q - What types of Project Node is great ?
+
+// Ans - 1 - Rest Api || 2 - Real time services like Chat App || 3 - CRUD Apps like Shopping Cart, Social media, Blogs.
+
+// Q - How to Set up a Node Run-time environment ?
+
+// Ans - sudo apt-get install -y nodejs
+
+// Q - What is NPM
+
+// Ans - npm is a node package manager which is used to generate a package of json file
+
+// Q - What is Node Modules 
+
+// Ans - Module in Node.js is a simple or complex functionality organized in single or multiple JavaScript files which can be reused throughout the Node.js application.
+
+// 
+
+ 
+
+
+
+
